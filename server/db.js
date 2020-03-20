04766b5068d5eb65bad178128e1d7c371b5dd128
Andrew Bateman <gomezbateman@yahoo.com>
2020-03-20 18:21:10 +0100

    initial commit - working backend, frontend template created

---
+++ server/db.js
@@ -0,0 +1,14 @@
+
+require('dotenv').config();
+
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.user,
+  password: process.env.password,
+  host: process.env.host,
+  port: process.env.port,
+  database: process.env.database
+});
+
+module.exports = pool;
